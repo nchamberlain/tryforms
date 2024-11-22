97361fca686ba2c6c9b07d56aa44fa4bbe329728
Nelson <nelson.chamberlain7@gmail.com>
2024-11-21 23:58:04 -0800

    Experimenting

--- main/forms.py
+++ main/forms.py
@@ -3,6 +3,7 @@ from django import forms
 #create your forms here.
 
 class ExampleForm(forms.Form):
-    name = forms.CharField()
-    comment = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(label="Enter Your Name")
+    comment = forms.CharField(widget=forms.Textarea(attrs={'rows':3}))
     date = forms.DateField()
+
